ac90d242e2cdf5f0cbebf8556ad605cdc810197a
jose sanchez <jose@joses-MacBook-Air.local>
2021-08-05 12:50:45 -0500

    stuff should be finished

--- java_project/consoleRewrite/src/App.java
+++ java_project/consoleRewrite/src/App.java
@@ -1,21 +1,24 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Car car = new Car();
         Grill grill = new Grill();
         Microwave microwave = new Microwave();
         House house = new House();
-
+        Console c = System.console();
         System.out.println("As you finish working, you return home. This is a retelling of the events that happen");
         car.decreaseRpm(1000);
         System.out.println("Your car has completely stopped. ");
         System.out.println("Do you want to open the door?");
-        if (System.in.read() == 'y') {
+        if (c.readLine().equalsIgnoreCase("y")) {
             car.openDoor();
             System.out.println("Alright. The door is open and now you're walking to your house.");
             car.closeDoor();
             System.out.println("You're now inside the house. Your car's door is closed.");
             System.out.println("Do you want to make lunch (and grill steak or something)?");
-            if (System.in.read() == 'y') {
+            if (c.readLine().equalsIgnoreCase("y")) {
                 grill.turnOn();
                 grill.increaseTemp(40);
                 grill.turnOff();
@@ -23,20 +26,22 @@ public class App {
 
             }
             System.out.println("Want to microwave chicken tendies?");
-            if (System.in.read() == 'y') {
+            if (c.readLine().equalsIgnoreCase("y")) {
                 microwave.turnOn();
                 microwave.increaseTemp(40);
                 microwave.turnOff();
                 System.out.println("You've successfully cooked the chicken tendies at: " + microwave.temperature);
             }
-            System.out.println("Your House is ugly! Want to renovate it and build more rooms?");
-            if (System.in.read() == 'y') {
+            System.out.println("Your house is ugly! Want to renovate it and build more rooms?");
+            if (c.readLine().equalsIgnoreCase("y")) {
                 System.out.println("How many rooms to build?");
-                // for (int i = 0; i < Integer.parseInt(System.console().readLine()); i++) {
-                //     house.buildRoom();
-                // }
-                System.out.println("Your house now has " + house.numRooms + " rooms.");
+                int rooms = Integer.parseInt(c.readLine());
+                for (int i = 0; i < rooms; i++) {
+                    house.buildRoom();
+                }
+                System.out.println("Your house now has " + house.numRooms + " rooms and isn't as ugly.");
             }
         }
     }
+
 }
