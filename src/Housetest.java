d57dec9e2d2a93a99f14c1a2468ccb8702a59a87
kayhaynes <87337140+kayhaynes@users.noreply.github.com>
2021-08-09 12:42:18 -0500

    test

---
+++ src/Housetest.java
@@ -0,0 +1,23 @@
+package com.senecafoundation;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+ Public class RoomTest {
+
+    private Room systemUnderTest;
+
+    @BeforeEach
+    void assignRoom() {
+        this.systemUnderTest = new Room(
+        );
+    }
+    @Test
+    void testRooms() {
+        this.systemUnderTest.renovate();
+        assertEquals(this.systemUnderTest.y, true);
+        this.systemUnderTest.();
+        assertEquals(this.systemUnderTest.n, false);
+    }
+    
