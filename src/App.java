afc9f249267f1a066a396ffc5088ea624123fdcf
jose sanchez <josersanchez0117@gmail.com>
2021-08-11 10:59:23 -0500

    feature: reorganized code into main class

---
+++ src/App.java
@@ -0,0 +1,47 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Car car = new Car();
+        Grill grill = new Grill();
+        Microwave microwave = new Microwave();
+        House house = new House();
+        Console c = System.console();
+        System.out.println("As you finish working, you return home. This is a retelling of the events that happen");
+        car.decreaseRpm(1000);
+        System.out.println("Your car has completely stopped. ");
+        System.out.println("Do you want to open the door?");
+        if (c.readLine().equalsIgnoreCase("y")) {
+            car.openDoor();
+            System.out.println("Alright. The door is open and now you're walking to your house.");
+            car.closeDoor();
+            System.out.println("You're now inside the house. Your car's door is closed.");
+            System.out.println("Do you want to make lunch (and grill steak or something)?");
+            if (c.readLine().equalsIgnoreCase("y")) {
+                grill.turnOn();
+                grill.increaseTemp(40);
+                grill.turnOff();
+                System.out.println("You've successfully cooked the meat at: " + grill.temperature);
+
+            }
+            System.out.println("Want to microwave chicken tendies?");
+            if (c.readLine().equalsIgnoreCase("y")) {
+                microwave.turnOn();
+                microwave.increaseTemp(40);
+                microwave.turnOff();
+                System.out.println("You've successfully cooked the chicken tendies at: " + microwave.temperature);
+            }
+            System.out.println("Your house is ugly! Want to renovate it and build more rooms?");
+            if (c.readLine().equalsIgnoreCase("y")) {
+                System.out.println("How many rooms to build?");
+                int rooms = Integer.parseInt(c.readLine());
+                for (int i = 0; i < rooms; i++) {
+                    house.buildRoom();
+                }
+                System.out.println("Your house now has " + house.numRooms + " rooms and isn't as ugly.");
+            }
+        }
+    }
+
+}
---
+++ src/Grill.java
@@ -0,0 +1,21 @@
+public class Grill {
+    public int temperature;
+    public boolean power;
+
+    public Grill() {
+        // temperature = temp;
+        // power = false;
+    }
+    public void increaseTemp(int temp) {
+        temperature += temp;
+    }
+    public void decreaseTemp(int temp) {
+        temperature -= temp;
+    }
+    public void turnOn() {
+        power = true;
+    }
+    public void turnOff() {
+        power = false;
+    }
+}
