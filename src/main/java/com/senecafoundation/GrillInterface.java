bdf35464869ea50041e1427994788479269c824a
kayhaynes <87337140+kayhaynes@users.noreply.github.com>
2021-08-13 14:56:18 -0500

    Added Interfaces

---
+++ src/main/java/com/senecafoundation/GrillInterface.java
@@ -0,0 +1,10 @@
+package com.senecafoundation;
+
+public interface GrillInterface {
+    public void increaseTemp(int temp);
+    public void decreaseTemp(int temp);
+    public void turnOn ();
+    public void turnOff ();
+}
+    
+
